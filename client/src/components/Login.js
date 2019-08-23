37703161597bacd8237815933b349d01fe94cd62
QuintonStudent <49251721+QuintonStudent@users.noreply.github.com>
2019-08-23 11:32:23 -0400

    retrieved token from the server and saved it to localStorage

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,8 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from 'axios';
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  useEffect(() => {
+    axios.post('http://localhost:5000/api/login', { username: 'Lambda School', password: 'i<3Lambd4' })
+      .then(response => {
+        console.log(response);
+        localStorage.setItem('token', response.data.payload);
+      })
+      .catch(error => {
+        console.log(error.response);
+      });
+  })
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
